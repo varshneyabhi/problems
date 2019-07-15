37c0a309a24f6e9352d604b0b3a7abf039e9f04b
varshneyabhi <varshneyabhi@users.noreply.github.com>
2019-07-15 19:12:47 +0530

    Create largestvalue.clj

---
+++ src/largestvalue.clj
@@ -0,0 +1,19 @@
+(ns largestvalue)
+
+(defn comp-< 
+  ;; Compare two numbers and return True/False as comparison.                                     
+  [a b]                                
+  (let [x (Integer/parseInt (str a b)) 
+        y (Integer/parseInt (str b a))]
+    (cond 
+      (> x y) true   
+      (= x y) (> a b)                                           
+      :else false))) 
+
+(defn create-largest-num    
+  ;; It takes a list and returns possible biggest number. e.g.
+  ;; [1 34 3 98 9 76 45 4] => 998764543431                           
+  [x]
+  (let [s (sort comp-< x)
+        p (reduce #(str %1 %2) "" s)]
+    (println p)))
